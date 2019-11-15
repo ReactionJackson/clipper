36dbaf78b66d2aeb610fd3c6ee5b0aefb48bbfac
Joe Jackson <joe.jackson@parall.ax>
2019-11-15 13:03:54 +0000

    added pie filling (needs refactoring but works for now)

--- src/components/Clipper.js
+++ src/components/Clipper.js
@@ -1,20 +1,41 @@
 import React, { useState, useRef, useEffect } from "react"
 import useAnimation from "../hooks/useAnimation"
 import useListener from "../hooks/useListener"
-
-const src = [
-	'https://images.unsplash.com/photo-1488741824574-82b39614583c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3450&q=80',
-	'https://inews.co.uk/images-i.jpimedia.uk/imagefetch/https://inews.co.uk/wp-content/uploads/2019/10/shutterstock_1479789158.jpg'
-]
+import useViewport from "../hooks/useViewport"
 
 const Clipper = () => {
 
 	let pos = { x: 0, y: 0 }
 	const [ drag, setDrag ] = useState(pos)
+	// const [ radius, setRadius ] = useState(100)
+	const [ path, setPath ] = useState("")
+	const { width, height } = useViewport()
 
 	useListener("mousemove", e => pos = { x: e.clientX, y: e.clientY })
 
+	let a = 0
+
+	var startPointX = 0
+	var startPointY = 0
+	var startAngle = -90
+	var endAngle = -90
+	var radius = 200
+
+	var x1 = startPointX + radius * Math.cos(Math.PI * startAngle / 180)
+	var y1 = startPointY + radius * Math.sin(Math.PI * startAngle / 180)
+	var x2 = startPointX + radius * Math.cos(Math.PI * endAngle / 180)
+	var y2 = startPointY + radius * Math.sin(Math.PI * endAngle / 180)
+
 	useAnimation(() => {
+
+		a = a + 1 > 360 ? 0 : a + 1
+		endAngle = -90 + a
+		x1 = startPointX + radius * Math.cos(Math.PI * startAngle / 180)
+		y1 = startPointY + radius * Math.sin(Math.PI * startAngle / 180)
+		x2 = startPointX + radius * Math.cos(Math.PI * endAngle / 180)
+		y2 = startPointY + radius * Math.sin(Math.PI * endAngle / 180)
+
+		setPath("M0,0 L" + x1 + "," + y1 + " A" + radius + "," + radius + " 0 " + (endAngle > 90 ? 1 : 0) + ",1 " + x2 + "," + y2 + " z")
 
 		setDrag(prev => ({
       x: prev.x + ((pos.x - prev.x) / 12),
@@ -25,12 +46,12 @@ const Clipper = () => {
 
 	return (
 
-		<svg viewBox={`0 0 ${ window.innerWidth } ${ window.innerHeight }`} width={ window.innerWidth } height={ window.innerHeight }>
+		<svg viewBox={`0 0 ${ width } ${ height }`} width={ width } height={ height }>
 			<clipPath id="circle">
-				<circle cx={ drag.x } cy={ drag.y } r="200" fill="skyblue" />
+				<path d={ path } fill="#c60f1d" transform={`translate(${ drag.x },${ drag.y })`} />
 			</clipPath>
-			<image xlinkHref={ src[0] } width={ window.innerWidth } height={ window.innerHeight } />
-			<image clipPath="url(#circle)" xlinkHref={ src[1] } width={ window.innerWidth } height={ window.innerHeight } />
+			<image xlinkHref={ require("../assets/img/1.png") } width={ width } height={ height } preserveAspectRatio="none" />
+			<image xlinkHref={ require("../assets/img/2.png") } width={ width } height={ height } preserveAspectRatio="none" clipPath="url(#circle)" />
 		</svg>
 	)
 }
